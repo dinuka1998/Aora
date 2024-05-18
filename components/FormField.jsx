491e37ee40b1e5944f61233654c935b7a26c5d56
Dinuka Amarasinghe <agpdinuka1998@gmail.com>
2024-05-18 18:25:44 +0530

    Auth Screen Form Element Created

---
+++ components/FormField.jsx
@@ -0,0 +1,41 @@
+import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
+import React, { useState } from "react";
+
+import { icons } from "../constants";
+
+const FormField = ({
+  title,
+  value,
+  placeholder,
+  handleChangeText,
+  otherStyles,
+  ...props
+}) => {
+  const [ShowPassowrd, setShowPassowrd] = useState(false);
+  return (
+    <View className={`space-y-2 ${otherStyles}`}>
+      <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
+      <View className="w-full h-16 px-4 border-black-200 bg-black-100 rounded-2xl focus:border-secondary items-center flex-row">
+        <TextInput
+          className="flex-1 text-white font-psemibold"
+          value={value}
+          placeholder={placeholder}
+          placeholderTextColor="#7b7b8b"
+          onChangeText={handleChangeText}
+          secureTextEntry={title === "Password" && !ShowPassowrd}
+        />
+
+        {title === "Password" && (
+          <TouchableOpacity onPress={() => setShowPassowrd(!ShowPassowrd)}>
+            <Image
+              source={!ShowPassowrd ? icons.eye : icons.eyeHide}
+              className="w-6 h-6"
+            />
+          </TouchableOpacity>
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default FormField;
